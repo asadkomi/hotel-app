cc1d8412c1e8542fef762e6e50b999d7d93ce21f
asadkomi@iastate.edu <asadkomi@iastate.edu>
2021-07-19 20:59:46 -0500

    db connected

--- pages/api/rooms/index.jsx
+++ pages/api/rooms/index.jsx
@@ -1,7 +1,10 @@
 import nextConnect from "next-connect";
+import dbConnect from "../../../Backend/database/dbConnect.jsx";
 import { getAllRooms } from "../../../Backend/api/roomController.jsx";
 
 const handler = nextConnect();
+dbConnect();
+
 handler.get(getAllRooms);
 
 export default handler;
